e5aef3656fe8955ad9dccb3738fffbb289c5ff54
pks9862728888 <pks9862728888@gmail.com>
2023-10-08 14:03:54 +0530

    Added plugin to detect version of the project where it is added

--- mvn-version-bumping-plugin/src/main/java/com/demo/mvnversionbumpingplugin/MvnVersionBumpingPluginApplication.java
+++ mvn-version-bumping-plugin/src/main/java/com/demo/mvnversionbumpingplugin/MvnVersionBumpingPluginApplication.java
@@ -1,13 +1,31 @@
 package com.demo.mvnversionbumpingplugin;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Mojo(name = "bumpAndTag", defaultPhase = LifecyclePhase.COMPILE)
 @SpringBootApplication
-public class MvnVersionBumpingPluginApplication {
+@Slf4j
+public class MvnVersionBumpingPluginApplication extends AbstractMojo {
+
+	@Parameter(defaultValue = "${project}", required = true, readonly = true)
+	private MavenProject mavenProject;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvnVersionBumpingPluginApplication.class, args);
 	}
 
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		log.info("{}", mavenProject.getVersion());
+	}
 }
